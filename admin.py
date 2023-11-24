b9b2c08900aa1679d317aeec3e05dcca63aa71c5
Ethan <epestellmoyer@gmail.com>
2023-11-24 15:24:26 -0500

    Finished admin

--- admin.py
+++ admin.py
@@ -1,4 +1,5 @@
 from PySide6 import QtCore, QtWidgets, QtSql
+from passenger import ModifyBookingDialog
 
 class AdminWindow(QtWidgets.QMainWindow):
     def __init__(self) -> None:
@@ -12,10 +13,10 @@ class AdminWindow(QtWidgets.QMainWindow):
         flightsTab = QtWidgets.QWidget()
         tabWidget.addTab(flightsTab, "Flights")
 
-        passengersTab = QtWidgets.QWidget()
+        passengersTab = QtWidgets.QTabWidget()
         tabWidget.addTab(passengersTab, "Passengers")
 
-        crewTab = QtWidgets.QWidget()
+        crewTab = QtWidgets.QTabWidget()
         tabWidget.addTab(crewTab, "Crew")
 
         # Flights tab
@@ -64,96 +65,387 @@ class AdminWindow(QtWidgets.QMainWindow):
         flightsVLayout.addLayout(flightsButtonLayout)
         flightsButtonLayout.addStretch()
 
-        self.flightDetailsButton = QtWidgets.QPushButton("View Details")
-        self.flightDetailsButton.clicked.connect(self.openDetailsDialog)
-        flightsButtonLayout.addWidget(self.flightDetailsButton)
+        self.addPassengerBookingButton = QtWidgets.QPushButton("Add Passenger Booking")
+        self.addPassengerBookingButton.clicked.connect(self.openPassengerBookingDialog)
+        flightsButtonLayout.addWidget(self.addPassengerBookingButton)
+
+        self.addCrewBookingButton = QtWidgets.QPushButton("Add Crew Booking")
+        self.addCrewBookingButton.clicked.connect(self.openCrewBookingDialog)
+        flightsButtonLayout.addWidget(self.addCrewBookingButton)
+
+        self.addFlightButton = QtWidgets.QPushButton("Add Flight")
+        self.addFlightButton.clicked.connect(self.openAddFlightDialog)
+        flightsButtonLayout.addWidget(self.addFlightButton)
+
+        self.modifyFlightButton = QtWidgets.QPushButton("Modify Flight")
+        self.modifyFlightButton.clicked.connect(self.openModifyFlightDialog)
+        flightsButtonLayout.addWidget(self.modifyFlightButton)
 
         self.removeFlightButton = QtWidgets.QPushButton("Remove Flight")
         self.removeFlightButton.clicked.connect(self.removeFlight)
         flightsButtonLayout.addWidget(self.removeFlightButton)
 
+        # Passenger accounts tab
+        passengerAccountsTab = QtWidgets.QWidget()
+        passengersTab.addTab(passengerAccountsTab, "Accounts")
+
+        paVLayout = QtWidgets.QVBoxLayout(passengerAccountsTab)
+        paHLayout = QtWidgets.QHBoxLayout()
+        paVLayout.addLayout(paHLayout)
+
+        paHLayout.addWidget(QtWidgets.QLabel("First Name:"))
+        self.paFirstNameEdit = QtWidgets.QLineEdit()
+        self.paFirstNameEdit.setMinimumWidth(150)
+        paHLayout.addWidget(self.paFirstNameEdit)
+
+        paHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        paHLayout.addWidget(QtWidgets.QLabel("Last Name:"))
+        self.paLastNameEdit = QtWidgets.QLineEdit()
+        self.paLastNameEdit.setMinimumWidth(150)
+        paHLayout.addWidget(self.paLastNameEdit)
+
+        paHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        paHLayout.addWidget(QtWidgets.QLabel("Date of Birth:"))
+        self.paDOBEdit = QtWidgets.QDateEdit(QtCore.QDate.currentDate())
+        self.paDOBEdit.setMinimumWidth(100)
+        paHLayout.addWidget(self.paDOBEdit)
+
+        paHLayout.addStretch()
+
+        self.paModel = QtSql.QSqlQueryModel()
+        self.paView = QtWidgets.QTableView()
+        self.paView.setModel(self.paModel)
+        paVLayout.addWidget(self.paView)
+
+        paButtonLayout = QtWidgets.QHBoxLayout()
+        paVLayout.addLayout(paButtonLayout)
+        paButtonLayout.addStretch()
+        self.deletePassengerButton = QtWidgets.QPushButton("Delete Account")
+        self.deletePassengerButton.clicked.connect(self.deletePassenger)
+        paButtonLayout.addWidget(self.deletePassengerButton)
+
+        # Passenger bookings tab
+        passengerBookingsTab = QtWidgets.QWidget()
+        passengersTab.addTab(passengerBookingsTab, "Bookings")
+
+        pbVLayout = QtWidgets.QVBoxLayout(passengerBookingsTab)
+        pbHLayout = QtWidgets.QHBoxLayout()
+        pbVLayout.addLayout(pbHLayout)
+
+        pbHLayout.addWidget(QtWidgets.QLabel("From:"))
+        self.pbFromDateEdit = QtWidgets.QDateEdit(QtCore.QDate.currentDate())
+        self.pbFromDateEdit.setMinimumWidth(100)
+        pbHLayout.addWidget(self.pbFromDateEdit)
+        self.pbFromCheckBox = QtWidgets.QCheckBox()
+
+        pbHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        pbHLayout.addWidget(QtWidgets.QLabel("To:"))
+        self.pbToDateEdit = QtWidgets.QDateEdit(QtCore.QDate.currentDate().addDays(14))
+        self.pbToDateEdit.setMinimumWidth(100)
+        pbHLayout.addWidget(self.pbToDateEdit)
+        self.pbToCheckBox = QtWidgets.QCheckBox()
+        pbHLayout.addWidget(self.pbToCheckBox)
+
+        pbHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        pbHLayout.addWidget(QtWidgets.QLabel("Airline:"))
+        self.pbAirlineComboBox = QtWidgets.QComboBox()
+        self.pbAirlineComboBox.setMinimumWidth(150)
+        pbHLayout.addWidget(self.pbAirlineComboBox)
+
+        pbHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        pbHLayout.addWidget(QtWidgets.QLabel("Destination:"))
+        self.pbDestinationLineEdit = QtWidgets.QLineEdit()
+        self.pbDestinationLineEdit.setFixedWidth(70)
+        pbHLayout.addWidget(self.pbDestinationLineEdit)
+
+        pbHLayout.addStretch()
+
+        self.pbModel = QtSql.QSqlQueryModel()
+        self.pbView = QtWidgets.QTableView()
+        self.pbView.setModel(self.pbModel)
+        pbVLayout.addWidget(self.pbView)
+
+        pbButtonLayout = QtWidgets.QHBoxLayout()
+        pbVLayout.addLayout(pbButtonLayout)
+        pbButtonLayout.addStretch()
+
+        self.pbModifyBookingButton = QtWidgets.QPushButton("Modify Booking")
+        self.pbModifyBookingButton.clicked.connect(self.modifyPassengerBooking)
+        pbButtonLayout.addWidget(self.pbModifyBookingButton)
+
+        self.pbRemoveBookingButton = QtWidgets.QPushButton("Remove Booking")
+        self.pbRemoveBookingButton.clicked.connect(self.removePassengerBooking)
+        pbButtonLayout.addWidget(self.pbRemoveBookingButton)
+
+        # Crew accounts tab
+        crewAccountsTab = QtWidgets.QWidget()
+        crewTab.addTab(crewAccountsTab, "Accounts")
+
+        caVLayout = QtWidgets.QVBoxLayout(crewAccountsTab)
+        caHLayout = QtWidgets.QHBoxLayout()
+        caVLayout.addLayout(caHLayout)
+
+        caHLayout.addWidget(QtWidgets.QLabel("First Name:"))
+        self.caFirstNameEdit = QtWidgets.QLineEdit()
+        self.caFirstNameEdit.setMinimumWidth(150)
+        caHLayout.addWidget(self.caFirstNameEdit)
+
+        caHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        caHLayout.addWidget(QtWidgets.QLabel("Last Name:"))
+        self.caLastNameEdit = QtWidgets.QLineEdit()
+        self.caLastNameEdit.setMinimumWidth(150)
+        caHLayout.addWidget(self.caLastNameEdit)
+
+        caHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        caHLayout.addWidget(QtWidgets.QLabel("Date of Birth:"))
+        self.caDOBEdit = QtWidgets.QDateEdit(QtCore.QDate.currentDate())
+        self.caDOBEdit.setMinimumWidth(100)
+        caHLayout.addWidget(self.caDOBEdit)
+
+        caHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        caHLayout.addWidget(QtWidgets.QLabel("Position:"))
+        self.caPositionEdit = QtWidgets.QLineEdit()
+        self.caPositionEdit.setMinimumWidth(150)
+        caHLayout.addWidget(self.caPositionEdit)
+
+        caHLayout.addStretch()
+
+        self.caModel = QtSql.QSqlQueryModel()
+        self.caView = QtWidgets.QTableView()
+        self.caView.setModel(self.caModel)
+        caVLayout.addWidget(self.caView)
+
+        caButtonLayout = QtWidgets.QHBoxLayout()
+        caVLayout.addLayout(caButtonLayout)
+        caButtonLayout.addStretch()
+
+        self.createCrewButton = QtWidgets.QPushButton("Create Account")
+        self.createCrewButton.clicked.connect(self.createCrew)
+        caButtonLayout.addWidget(self.createCrewButton)
+
+        self.deleteCrewButton = QtWidgets.QPushButton("Delete Account")
+        self.deleteCrewButton.clicked.connect(self.deleteCrew)
+        caButtonLayout.addWidget(self.deleteCrewButton)
+
+        # Crew bookings tab
+        crewBookingsTab = QtWidgets.QWidget()
+        crewTab.addTab(crewBookingsTab, "Bookings")
+
+        cbVLayout = QtWidgets.QVBoxLayout(crewBookingsTab)
+        cbHLayout = QtWidgets.QHBoxLayout()
+        cbVLayout.addLayout(cbHLayout)
+
+        cbHLayout.addWidget(QtWidgets.QLabel("From:"))
+        self.cbFromDateEdit = QtWidgets.QDateEdit(QtCore.QDate.currentDate())
+        self.cbFromDateEdit.setMinimumWidth(100)
+        cbHLayout.addWidget(self.cbFromDateEdit)
+        self.cbFromCheckBox = QtWidgets.QCheckBox()
+
+        cbHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        cbHLayout.addWidget(QtWidgets.QLabel("To:"))
+        self.cbToDateEdit = QtWidgets.QDateEdit(QtCore.QDate.currentDate().addDays(14))
+        self.cbToDateEdit.setMinimumWidth(100)
+        cbHLayout.addWidget(self.cbToDateEdit)
+        self.cbToCheckBox = QtWidgets.QCheckBox()
+        cbHLayout.addWidget(self.cbToCheckBox)
+
+        cbHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        cbHLayout.addWidget(QtWidgets.QLabel("Airline:"))
+        self.cbAirlineComboBox = QtWidgets.QComboBox()
+        self.cbAirlineComboBox.setMinimumWidth(150)
+        cbHLayout.addWidget(self.cbAirlineComboBox)
+
+        cbHLayout.addSpacerItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Preferred))
+
+        cbHLayout.addWidget(QtWidgets.QLabel("Destination:"))
+        self.cbDestinationLineEdit = QtWidgets.QLineEdit()
+        self.cbDestinationLineEdit.setFixedWidth(70)
+        cbHLayout.addWidget(self.cbDestinationLineEdit)
+
+        cbHLayout.addStretch()
+
+        self.cbModel = QtSql.QSqlQueryModel()
+        self.cbView = QtWidgets.QTableView()
+        self.cbView.setModel(self.cbModel)
+        cbVLayout.addWidget(self.cbView)
+
+        cbButtonLayout = QtWidgets.QHBoxLayout()
+        cbVLayout.addLayout(cbButtonLayout)
+        cbButtonLayout.addStretch()
+        self.cbRemoveBookingButton = QtWidgets.QPushButton("Remove Booking")
+        self.cbRemoveBookingButton.clicked.connect(self.removeCrewBooking)
+        cbButtonLayout.addWidget(self.cbRemoveBookingButton)
+
         self.resize(1120, 590)
 
     @QtCore.Slot()
-    def openDetailsDialog(self) -> None:
-        details = FlightDetailsDialog()
-        details.exec()
+    def openAddFlightDialog(self) -> None:
+        dialog = AddFlightDialog(False)
+        dialog.exec()
+
+    @QtCore.Slot()
+    def openModifyFlightDialog(self) -> None:
+        dialog = AddFlightDialog(True)
+        dialog.exec()
 
     @QtCore.Slot()
     def removeFlight(self) -> None:
         print("Remove flight here")
+    
+    @QtCore.Slot()
+    def deletePassenger(self) -> None:
+        print("Delete passenger here")
 
-class FlightDetailsDialog(QtWidgets.QDialog):
+    @QtCore.Slot()
+    def openPassengerBookingDialog(self) -> None:
+        dialog = AddPassengerBookingDialog()
+        dialog.exec()
+
+    @QtCore.Slot()
+    def openCrewBookingDialog(self) -> None:
+        dialog = AddCrewBookingDialog()
+        dialog.exec()
+
+    @QtCore.Slot()
+    def modifyPassengerBooking(self) -> None:
+        dialog = ModifyBookingDialog()
+        dialog.exec()
+
+    @QtCore.Slot()
+    def removePassengerBooking(self) -> None:
+        print("Remove booking here")
+
+    @QtCore.Slot()
+    def createCrew(self) -> None:
+        dialog = CreateCrewDialog()
+        dialog.exec()
+
+    @QtCore.Slot()
+    def deleteCrew(self) -> None:
+        print("Delete crew here")
+
+    @QtCore.Slot()
+    def removeCrewBooking(self) -> None:
+        print("Remove booking here")
+
+class AddFlightDialog(QtWidgets.QDialog):
+    def __init__(self, modifyingFlight) -> None:
+        super().__init__()
+
+        if not modifyingFlight:
+            self.setWindowTitle("Airport Manager - Add Flight")
+        else:
+            self.setWindowTitle("Airport Manager - Modify Flight")
+
+        self.layout = QtWidgets.QFormLayout(self)
+
+        self.planeComboBox = QtWidgets.QComboBox()
+        self.layout.addRow("Plane:", self.planeComboBox)
+
+        self.depTermLineEdit = QtWidgets.QLineEdit()
+        self.layout.addRow("Departure Terminal:", self.depTermLineEdit)
+
+        self.depDateTimeEdit = QtWidgets.QDateTimeEdit(QtCore.QDateTime.currentDateTime())
+        self.layout.addRow("Departure Time:", self.depDateTimeEdit)
+
+        self.arrDateTimeEdit = QtWidgets.QDateTimeEdit(QtCore.QDateTime.currentDateTime())
+        self.layout.addRow("Arrival Time:", self.arrDateTimeEdit)
+
+        self.destLineEdit = QtWidgets.QLineEdit()
+        self.destLineEdit.setMaxLength(3)
+        self.layout.addRow("Destination Airport:", self.destLineEdit)
+
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok, QtCore.Qt.Horizontal)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+        self.layout.addRow(buttonBox)
+
+        self.resize(380, 180)
+
+class AddPassengerBookingDialog(QtWidgets.QDialog):
     def __init__(self) -> None:
         super().__init__()
 
-        self.setWindowTitle("Airport Manager - Flight Details")
+        self.setWindowTitle("Airport Manager - Add Passenger Booking")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout = QtWidgets.QFormLayout(self)
 
-        self.layout.addWidget(QtWidgets.QLabel("Flight ID: "))
-        self.layout.addWidget(QtWidgets.QLabel("Airline: "))
-        self.layout.addWidget(QtWidgets.QLabel("Plane: "))
-        self.layout.addWidget(QtWidgets.QLabel("Terminal: "))
-        self.layout.addWidget(QtWidgets.QLabel("Departure Date: "))
-        self.layout.addWidget(QtWidgets.QLabel("Departure Time: "))
-        self.layout.addWidget(QtWidgets.QLabel("Arrival Time: "))
-        self.layout.addWidget(QtWidgets.QLabel("Destination: "))
+        self.passengersComboBox = QtWidgets.QComboBox()
+        self.layout.addRow("Passenger:", self.passengersComboBox)
 
-        self.layout.addStretch()
+        self.seatNumComboBox = QtWidgets.QComboBox()
+        self.layout.addRow("Seat:", self.seatNumComboBox)
 
-        self.layout.addWidget(QtWidgets.QLabel("Passengers:"))
-        passengersModel = QtSql.QSqlQueryModel()
-        passengersView = QtWidgets.QTableView()
-        passengersView.setModel(passengersModel)
-        self.layout.addWidget(passengersView)
+        self.bagNumComboBox = QtWidgets.QComboBox()
+        for i in range(0, 3):
+            self.bagNumComboBox.addItem(str(i))
+        self.layout.addRow("Number of bags:", self.bagNumComboBox)
 
-        passengersButtonLayout = QtWidgets.QHBoxLayout()
-        self.layout.addLayout(passengersButtonLayout)
-        passengersButtonLayout.addStretch()
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok, QtCore.Qt.Horizontal)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+        self.layout.addRow(buttonBox)
 
-        self.addPassengerButton = QtWidgets.QPushButton("Add Passenger")
-        self.addPassengerButton.clicked.connect(self.addPassenger)
-        passengersButtonLayout.addWidget(self.addPassengerButton)
+        self.resize(280, 100)
 
-        self.removePassengerButton = QtWidgets.QPushButton("Remove Passenger")
-        self.removePassengerButton.clicked.connect(self.removePassenger)
-        passengersButtonLayout.addWidget(self.removePassengerButton)
-        
-        self.layout.addStretch()
+class CreateCrewDialog(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
 
-        self.layout.addWidget(QtWidgets.QLabel("Crew:"))
-        crewModel = QtSql.QSqlQueryModel()
-        crewView = QtWidgets.QTableView()
-        crewView.setModel(crewModel)
-        self.layout.addWidget(crewView)
+        self.setWindowTitle("Airport Manager - Create Crew")
 
-        crewButtonLayout = QtWidgets.QHBoxLayout()
-        self.layout.addLayout(crewButtonLayout)
-        crewButtonLayout.addStretch()
+        self.layout = QtWidgets.QFormLayout(self)
 
-        self.addCrewButton = QtWidgets.QPushButton("Add Crew")
-        self.addCrewButton.clicked.connect(self.addCrew)
-        crewButtonLayout.addWidget(self.addCrewButton)
+        self.firstNameLineEdit = QtWidgets.QLineEdit()
+        self.layout.addRow("First Name:", self.firstNameLineEdit)
 
-        self.removeCrewButton = QtWidgets.QPushButton("Remove Crew")
-        self.removeCrewButton.clicked.connect(self.removeCrew)
-        crewButtonLayout.addWidget(self.removeCrewButton)
+        self.lastNameLineEdit = QtWidgets.QLineEdit()
+        self.layout.addRow("Last Name:", self.lastNameLineEdit)
 
-        self.resize(450, 630)
+        self.dobDateEdit = QtWidgets.QDateEdit()
+        self.layout.addRow("Date of Birth:", self.dobDateEdit)
+
+        self.positionEdit = QtWidgets.QLineEdit()
+        self.layout.addRow("Position:", self.positionEdit)
+
+        self.signUpUsernameLineEdit = QtWidgets.QLineEdit()
+        self.layout.addRow("Username:", self.signUpUsernameLineEdit)
+
+        self.signUpPasswordLineEdit = QtWidgets.QLineEdit()
+        self.signUpPasswordLineEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.layout.addRow("Password:", self.signUpPasswordLineEdit)
+
+        createAccountButton = QtWidgets.QPushButton("Create Account")
+        createAccountButton.clicked.connect(self.createAccountButtonClicked)
+        self.layout.addRow(createAccountButton)
 
     @QtCore.Slot()
-    def addPassenger(self) -> None:
-        print("Add passenger here")
+    def createAccountButtonClicked(self) -> None:
+        print("Create account here")
+        self.accept()
 
-    @QtCore.Slot()
-    def removePassenger(self) -> None:
-        print("Remove passenger here")
+class AddCrewBookingDialog(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
 
-    @QtCore.Slot()
-    def addCrew(self) -> None:
-        print("Add crew here")
+        self.setWindowTitle("Airport Manager - Add Crew Booking")
 
-    @QtCore.Slot()
-    def removeCrew(self) -> None:
-        print("Remove crew here")
+        self.layout = QtWidgets.QFormLayout(self)
+
+        self.crewComboBox = QtWidgets.QComboBox()
+        self.layout.addRow("Crew Member:", self.crewComboBox)
+
+        buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok, QtCore.Qt.Horizontal)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+        self.layout.addRow(buttonBox)
